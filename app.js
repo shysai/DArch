6a5c4cbf15ad725e6c463bdfa62ddc7042fa18d5
syam_sai <konchadashyamsai@gmail.com>
2025-08-02 22:27:14 +0530

    Fix: Correct app.js port specification

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
